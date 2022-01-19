519f3f5808b63a824cc25959154e6cacfe157c5f
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2022-01-19 14:28:29 -0500

    add v3 - emojis

---
+++ v3/Card.java
@@ -0,0 +1,43 @@
+// chrysanthemumtea: Ryan Lau, Melody Lew, Anthony Sun
+// apcs pd6
+// fp: tarot card readings
+// 2022-01-21f
+// time spent: 5.5 hours
+
+public class Card {
+    private String name, suit, desc, upright, reversed;
+
+    public Card(String name, String suit, String desc, String upright, String reversed) {
+        this.name = name;
+        this.suit = suit;
+        this.desc = desc;
+        this.upright = upright;
+        this.reversed = reversed;
+    }
+
+    /*
+    public String getName() {
+        return name;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getUpright() {
+        return upright;
+    }
+
+    public String getReversed() {
+        return reversed;
+    }
+    */
+
+    public String toString() {
+        return name + " " + suit + " " + desc + " " + upright + " " + reversed;
+    }
+}
