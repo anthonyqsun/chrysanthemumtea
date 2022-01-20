96f32d3d9c31a9659ae8ce2313efbea532d05a4c
anthonyqsun <qsun30@stuy.edu>
2022-01-19 19:36:28 -0500

    add tester file

---
+++ v1/Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args) {
+        Appointment app = new Appointment("a");
+        System.out.println(app.parseTime("12:34 pm"));
+    }
+}
